3ad969c0ebf1c41a742c4932f571d1873a9b1bff
888-coding <pachelee@gmail.com>
2023-02-15 23:47:33 -0300

    First condition added

--- main.py
+++ main.py
@@ -4,3 +4,11 @@
 print("Welcome to the Rollercoaster!")
 age = int(input("Please enter your age: "))
 height = float(input("How tall are you (cm) ? "))
+
+#Conditions
+## If less than 120 cm , you cant play
+
+if height >= 120:
+    print()
+else:
+    print("You don't have height to play this.")
