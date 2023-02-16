76100d306f21715da447c28ec252056d54c1d754
888-coding <pachelee@gmail.com>
2023-02-16 06:48:00 -0300

    Add the ticket normal price

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ price = 0
 ## If less than 120 cm , you cant play
 ## If you are between 18 to 49 years old, you pay 10 more bucks 
 ## If you want photo, add 10 bucks.
+## Ticket normal price is 15 
 
 if height >= 120:
+    price_initial = 15
+    price = price_initial
     if age >= 18 and age <= 49:
         price += 10
     
@@ -19,6 +22,6 @@ if height >= 120:
     if wantPhoto == "y":
         price += 10
     
-    
+    print(f"Your ticket is going to cost : {price}")    
 else:
     print("You don't have height to play this.")
