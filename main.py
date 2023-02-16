20751858027e8af8e91eb279ba6c7bbf4ef2b803
888-coding <pachelee@gmail.com>
2023-02-15 23:58:02 -0300

    Added more conditions

--- main.py
+++ main.py
@@ -4,11 +4,21 @@
 print("Welcome to the Rollercoaster!")
 age = int(input("Please enter your age: "))
 height = float(input("How tall are you (cm) ? "))
+price = 0 
 
 #Conditions
 ## If less than 120 cm , you cant play
+## If you are between 18 to 49 years old, you pay 10 more bucks 
+## If you want photo, add 10 bucks.
 
 if height >= 120:
-    print()
+    if age >= 18 and age <= 49:
+        price += 10
+    
+    wantPhoto = str(input("Do you want photo (y/n)? ")).lower()
+    if wantPhoto == "y":
+        price += 10
+    
+    
 else:
     print("You don't have height to play this.")
